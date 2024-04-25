f42baa167cde86e017225d18fbcfd1d17c7f1af1
Clifton <cliftonreed1986@gmail.com>
2024-04-25 02:10:51 -0700

    cleaned up and fixed directory

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
-const router = require("express") . Router();
-const store = require(" .. /db/store");
+const router = require("express").Router();
+const store = require("../db/store");
 const path = require("path");
 
-const store = require("./db/store")
+
 // API Routes
-app.get("/api/notes", (req, res) => {
+router.get("/api/notes", (req, res) => {
     store.getNotes().then((notes) => {
         return res.status(200).json(notes)
     }).catch((error) => res.status(500).json(error))
 })
 
-app.post("/api/notes", (req, res) => {
+router.post("/api/notes", (req, res) => {
     store.addNotes(req.body).then((note) => {
         return res.status(200).json(note)
     }).catch((error) => res.status(500).json(error))
 })
 
 
-app.delete("/api/notes/:id", (req, res) => {
+router.delete("/api/notes/:id", (req, res) => {
     const noteId = req.params.id;
     store.removeNotes(noteId)
         .then(() => res.status(204).send()) // Send a 204 (No Content) response on successful deletion
@@ -25,12 +25,12 @@ app.delete("/api/notes/:id", (req, res) => {
 });
 
 // HTML ROUTES
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
     module. exports = router;
